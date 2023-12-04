a6a745ce25c5a6c24eb05d106cfda3d6130713a1
PeeraponGongthong <152461229+PeeraponGongthong@users.noreply.github.com>
2023-12-04 19:46:22 +0700

    login.html login.css หน้าloginกดปุ่มไปยังหน้าหลัก
    login.html login.css หน้าloginกดปุ่มไปยังหน้าหลัก
    
    Co-Authored-By: RaywatSupaka <152259102+RaywatSupaka@users.noreply.github.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ResgistComponent } from './resgist/resgist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path:'',
+    component:LoginComponent,
+  },{
+    path:'A',
+    component:ResgistComponent
+  },
+  {
+    path:'C',
+    component:ResgistComponent,
+    children:[
+      {
+        path:'A',
+        component:LoginComponent
+      }
+    ]
+  }
+ 
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
