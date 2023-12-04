5679ec6e45f60a38e7b1290db867520b993f24f0
PeeraponGongthong <152461229+PeeraponGongthong@users.noreply.github.com>
2023-12-04 20:31:52 +0700

    Revert "login.html login.css หน้าloginกดปุ่มไปยังหน้าหลัก"
    This reverts commit a6a745ce25c5a6c24eb05d106cfda3d6130713a1.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,30 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { ResgistComponent } from './resgist/resgist.component';
 
-const routes: Routes = [
-
-  {
-    path:'',
-    component:LoginComponent,
-  },{
-    path:'A',
-    component:ResgistComponent
-  },
-  {
-    path:'C',
-    component:ResgistComponent,
-    children:[
-      {
-        path:'A',
-        component:LoginComponent
-      }
-    ]
-  }
- 
-];
-
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
